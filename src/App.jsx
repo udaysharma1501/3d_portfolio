2280b5ad6b4fde54be1897f3f6b1fb7483056614
udaysharma1501 <udaysharma1501@gmail.com>
2025-01-12 11:02:08 +0530

    basic project complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ const App = () => {
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          
           <Navbar/>
           <Hero/>
-        
         </div>
       
         <About/>
